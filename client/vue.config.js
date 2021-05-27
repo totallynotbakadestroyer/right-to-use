15e2dc21941775ef713c648b760af2b45781fb4e
totallynotbakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2021-05-28 00:46:04 +0300

    Set up build scripts

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    proxy: 'http://localhost:3001'
-  }
+    proxy: "http://localhost:3001",
+  },
+  outputDir: path.resolve(__dirname, "../server/public"),
 };
